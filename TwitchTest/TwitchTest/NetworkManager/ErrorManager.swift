8d65a90d745d50dc0ee71ac147d55698111692e4
sergs582 <kurba37@gmail.com>
2020-05-29 09:58:26 +0300

    Add NetworkManager

---
+++ TwitchTest/TwitchTest/NetworkManager/ErrorManager.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorManager.swift
+//  TwitchTest
+//
+//  Created by Сергей on 29.05.2020.
+//  Copyright © 2020 Sunmait. All rights reserved.
+//
+
+import Foundation
+
+public let TWTNetworkingErrorDomain = "com.sunmait.TwitchTest.NetworkingError"
+public let MissingHTTPREsponseError = 100
+public let UnexpectedResponseError = 200
