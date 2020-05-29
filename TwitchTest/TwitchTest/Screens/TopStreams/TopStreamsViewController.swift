9735b9231443456d22cb9baa59c33604e1c891fb
sergs582 <kurba37@gmail.com>
2020-05-29 09:17:02 +0300

    Refactor main screen to TabBarController

---
+++ TwitchTest/TwitchTest/Screens/TopStreams/TopStreamsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TopStreamsViewController.swift
+//  TwitchTest
+//
+//  Created by Сергей on 29.05.2020.
+//  Copyright © 2020 Sunmait. All rights reserved.
+//
+
+import UIKit
+
+class TopStreamsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TwitchTest/TwitchTest/Screens/TopStreams/TopStreamsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TopStreamsViewModel.swift
+//  TwitchTest
+//
+//  Created by Сергей on 29.05.2020.
+//  Copyright © 2020 Sunmait. All rights reserved.
+//
+
+import Foundation
