6d68b59eb8d18046a16d22c45199b67c72c95ed6
sergs582 <kurba37@gmail.com>
2020-05-29 09:20:50 +0300

    Initialize Game TableView Cell

---
+++ TwitchTest/TwitchTest/Screens/GameCell/GameTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GameTableViewCell.swift
+//  TwitchTest
+//
+//  Created by Сергей on 29.05.2020.
+//  Copyright © 2020 Sunmait. All rights reserved.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
