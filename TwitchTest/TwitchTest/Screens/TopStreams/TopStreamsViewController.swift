27cf5002f7a89fe66fdba80a3c7b6faa4d0c4d4f
sergs582 <kurba37@gmail.com>
2020-05-29 09:47:46 +0300

    Setup TopStreams screen

--- TwitchTest/TwitchTest/Screens/TopStreams/TopStreamsViewController.swift
+++ TwitchTest/TwitchTest/Screens/TopStreams/TopStreamsViewController.swift
@@ -10,21 +10,29 @@ import UIKit
 
 class TopStreamsViewController: UIViewController {
 
+    @IBOutlet weak var streamsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupStreamsTable()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupStreamsTable() {
+        streamsTable.dataSource = self
+        streamsTable.register(UINib(nibName: "GameTableViewCell", bundle: nil), forCellReuseIdentifier: "gamecell")
     }
-    */
 
+}
+
+extension TopStreamsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gamecell") as! GameTableViewCell
+        cell.lTitle.text = "\(indexPath.row)"
+        return cell
+    }
+    
 }
